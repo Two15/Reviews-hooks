35cd790a33638eb42f5775d6f7f1171fc0efa503
Xavier Cambar <xcambar@gmail.com>
2016-03-30 08:08:32 -0700

    First commit - Works without any guarantee

---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+'use strict';
+
+const restify = require('restify');
+const eventFilter = require('./lib/event_filter');
+const actionFilter = require('./lib/action_filter');
+
+function handlerPOST(req, res, next) {
+  const event = req.headers['x-github-event'];
+  eventFilter(event)
+  .then(function(eventName) {
+    return actionFilter(eventName, req.body.action);
+  })
+  .then(function(fn) {
+    return fn(req.body);
+  })
+  .then(function() {
+    console.log('Action handled!');
+    res.send(201, 'OK');
+  }, function(reason) {
+    console.log('Something went wrong:', reason);
+    res.send(422, reason);
+  }).finally(function() {
+    next();
+  });
+}
+
+const server = restify.createServer();
+server.use(restify.bodyParser( { mapParams: false } ));
+
+server.post('/handler', handlerPOST);
+
+server.listen(8080, function() {
+  console.log(
+    '%s listening at %s',
+    server.name,
+    server.url
+  );
+});

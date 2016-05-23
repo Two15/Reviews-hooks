3dbe9acb301d1f65eddc87a9989d642a1ba4bfbb
Xavier Cambar <xcambar@gmail.com>
2016-05-23 09:20:26 +0200

    Fix forced reviews

--- test/acceptance/create_comment_test.js
+++ test/acceptance/create_comment_test.js
@@ -75,8 +75,98 @@ describeAPI('Adding a comment to a pull request', function() {
     return this.webhook("issue_comment", data);
   });
 
-  it('can accept a PR');
-  it('can reject a PR');
-  it('can force a rejection on behalf of another user');
-  it('can force a validation on behalf of another user');
+  it('can accept a PR', function() {
+    this.ghMock('GET', '/repos/Two15/trashbin/pulls/33',
+      require('./samples/github_api_pull_request_33.json'));
+    this.ghMock('GET',
+      '/repos/Two15/trashbin/commits/e1f846155e77fc33c36f3eef247dddc4477328f5/statuses',
+      [{
+        state: 'pending',
+        context: 'Review my code - @buddy'
+      }]);
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5',
+      (uri, data)=> {
+        assert.equal(data.context, 'Review my code - @buddy', 'Creates a review status for the user');
+        assert.equal(data.state, 'success', 'The status is set as a success');
+      }
+    );
+    // This creates the global status
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5');
+
+    const data = require('./samples/create_pull_request_comment_accept.json');
+    return this.webhook("issue_comment", data);
+  });
+
+  it('can reject a PR', function() {
+    this.ghMock('GET', '/repos/Two15/trashbin/pulls/33',
+      require('./samples/github_api_pull_request_33.json'));
+    this.ghMock('GET',
+      '/repos/Two15/trashbin/commits/e1f846155e77fc33c36f3eef247dddc4477328f5/statuses',
+      [{
+        state: 'pending',
+        context: 'Review my code - @buddy'
+      }]);
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5',
+      (uri, data)=> {
+        assert.equal(data.context, 'Review my code - @buddy', 'Creates a review status for the user');
+        assert.equal(data.state, 'failure', 'The status is set as a failure');
+      }
+    );
+    // This creates the global status
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5');
+
+    const data = require('./samples/create_pull_request_comment_reject.json');
+    return this.webhook("issue_comment", data);
+  });
+
+  it('can force a rejection on behalf of another user', function() {
+    this.ghMock('GET', '/repos/Two15/trashbin/pulls/33',
+      require('./samples/github_api_pull_request_33.json'));
+    this.ghMock('GET',
+      '/repos/Two15/trashbin/commits/e1f846155e77fc33c36f3eef247dddc4477328f5/statuses',
+      [{
+        state: 'pending',
+        context: 'Review my code - @buddy'
+      }]);
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5',
+      (uri, data)=> {
+        assert.equal(data.context, 'Review my code - @buddy', 'Creates a review status for the user');
+        assert.equal(data.state, 'failure', 'The status is set as a failure');
+      }
+    );
+    // This creates the global status
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5');
+
+    const data = require('./samples/create_pull_request_comment_force_reject.json');
+    return this.webhook("issue_comment", data);
+  });
+  it('can force a validation on behalf of another user', function() {
+    this.ghMock('GET', '/repos/Two15/trashbin/pulls/33',
+      require('./samples/github_api_pull_request_33.json'));
+    this.ghMock('GET',
+      '/repos/Two15/trashbin/commits/e1f846155e77fc33c36f3eef247dddc4477328f5/statuses',
+      [{
+        state: 'pending',
+        context: 'Review my code - @buddy'
+      }]);
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5',
+      (uri, data)=> {
+        assert.equal(data.context, 'Review my code - @buddy', 'Creates a review status for the user');
+        assert.equal(data.state, 'success', 'The status is set as a failure');
+      }
+    );
+    // This creates the global status
+    this.ghMock('POST',
+      '/repos/Two15/trashbin/statuses/e1f846155e77fc33c36f3eef247dddc4477328f5');
+
+    const data = require('./samples/create_pull_request_comment_force_accept.json');
+    return this.webhook("issue_comment", data);
+  });
 });
